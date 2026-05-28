43f39c57db4c8d3b4d4ecf5dcd7e45713d4202cb
Saloni Agarwal <agrawalsaloni443@gmail.com>
2026-05-28 23:26:06 +0530

    Add files via upload

---
+++ summer_assisgnment/Day1/ques3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+
+    int num, factorial = 1, i;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    for (i=1; i<=num; i++) {
+
+        factorial = factorial * i;
+        
+    }
+    printf("Factorial of %d = %d", num, factorial);
+
+
+    return 0;
+
+}
