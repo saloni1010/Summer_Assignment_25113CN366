a7033d729498e85d3c8d8320cdef98fcdd5c2537
Saloni Agarwal <agrawalsaloni443@gmail.com>
2026-06-25 00:03:30 +0530

    Add files via upload
    day 24

---
+++ summer_assisgnment/Day24/ques93.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int isRotation(char str1[], char str2[]) {
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+    if (len1 != len2)
+        return 0;
+
+    char temp[200];
+    strcpy(temp, str1);
+    strcat(temp, str1);
+
+    return (strstr(temp, str2) != NULL);
+}
+
+int main() {
+    char str1[100], str2[100];
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    if (isRotation(str1, str2))
+        printf("Strings are rotations of each other.\n");
+    else
+        printf("Strings are not rotations of each other.\n");
+
+    return 0;
+}
